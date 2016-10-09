7a3452f7659ff9a493af1c8d2bdd29b33b89ee60
qeesung <1245712564@qq.com>
2016-10-09 23:13:49 +0800

    add main test file

---
+++ src/decorator_pattern/sample/Main.java
@@ -0,0 +1,21 @@
+package decorator_pattern.sample;
+
+import decorator_pattern.sample.coffee.Espresso;
+import decorator_pattern.sample.decorator.Milk;
+import decorator_pattern.sample.decorator.Mocha;
+
+/**
+ * Created by qeesung on 2016/10/9.
+ */
+public class Main {
+    public static void main(String ... args) {
+        Baverage baverage1 = new Espresso();
+        System.out.println("cost is " + baverage1.cost());
+        System.out.println("> add some mocha");
+        baverage1 = new Mocha(baverage1);
+        System.out.println("cost is " + baverage1.cost());
+        System.out.println("> add some milk");
+        baverage1 = new Milk(baverage1);
+        System.out.println("cost is " + baverage1.cost());
+    }
+}
